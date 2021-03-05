787e6df38cf57459c813800dd2cb9c2c4f534675
wusheng <wxgdagege@gmail.com>
2021-03-05 20:36:19 +0800

    htmlwebpackplugin插件

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 import _ from "lodash";
-import "./style.css";
-import icon from "./icon.jpg";
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from "./data.toml";
-
+import printMe from './print.js';
 function component() {
     const element = document.createElement('div');
-  
+    const btn = document.createElement('button');
     // lodash（目前通过一个 script 引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add("hello");
-    
-    const myIocn = new Image();
-    myIocn.src = icon;
-    element.appendChild(myIocn);
 
-    console.log(Data);
-    console.log(Notes);
-    console.log(toml);
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
     return element;
   }
   
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,17 @@
 const path = require("path");
-const toml = require("toml");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, "dist"),
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js'
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource"
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: "asset/resource"
-            },
-            {
-                test: /\.(csv|tsv)$/i,
-                use: ["csv-loader"]
-            },
-            {
-                test: /\.xml$/i,
-                use: ["xml-loader"]
-            },
-            {
-                test: /\.toml/i,
-                type: "json",
-                parser: {
-                    parse: toml.parse
-                }
-            }
-        ]
-    }
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: '管理输出'
+        })
+    ]
 }
