1f1d44b64b4b59038079bcf2b17bf1a487168ffe
wusheng <wxgdagege@gmail.com>
2021-03-05 22:05:53 +0800

    选择开发工具：观察者模式，webpack-dev-server模式, webpack-dev-middleware模式（此模式使用nodejs 的express模块）

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,24 @@ const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
+    mode: 'development',
     entry: {
         index: './src/index.js',
         print: './src/print.js'
     },
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, "dist")
+        path: path.resolve(__dirname, "dist"),
+        publicPath: '/'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: '管理输出'
+            title: 'Development'
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin({cleanStaleWebpackAssets:false})
     ]
 }
