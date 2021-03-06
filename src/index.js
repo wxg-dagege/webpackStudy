2183685c3ff187b74f93a92a4ef5b24477736757
wusheng <wxgdagege@gmail.com>
2021-03-06 20:06:48 +0800

    动态导入 two 其实和上个版本一样只是换了写法

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-function getComponent() {
+async function getComponent() {
   const element = document.createElement('div');
-
-  return import('lodash')
-    .then(({default:_}) => {
-      const element = document.createElement('div');
-
-      element.innerHTML = _.join(['HELLO',"webpack"],"");
-
-      return element;
-    })
-    .catch((error) => 'An error occurred while loading the component')
+  const { default:_} = await import('lodash');
+  element.innerHTML = _.join(['HELLO','WEBPACK'],'');
+  return element;
 }
 getComponent().then((component) => {
   document.body.appendChild(component);
